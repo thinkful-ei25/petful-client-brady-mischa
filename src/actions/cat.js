2a4b5f9e87343a6ed80105d68ed6d19db6bad9e5
Box <40639989+theBoxed@users.noreply.github.com>
2018-12-21 12:18:25 -0500

    created fetch and adopt actions

--- src/actions/cat.js
+++ src/actions/cat.js
@@ -1,9 +1,8 @@
-
 //TODO: NEED TO ADD THE API_BASE_URL TO CONFIG
 import { API_BASE_URL } from '../config';
 
-export const  FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS';
-export const fetchCatSuccess = (results) => ({
+export const FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS';
+export const fetchCatSuccess = results => ({
   type: FETCH_CAT_SUCCESS,
   results
 });
@@ -14,45 +13,59 @@ export const fetchCatRequest = () => ({
 });
 
 export const FETCH_CAT_ERROR = 'FETCH_CAT_ERROR';
-export const fetchCatError = (error) => ({
+export const fetchCatError = error => ({
   type: FETCH_CAT_ERROR,
   error
 });
 
 export const fetchCat = () => {
-  return(dispatch) => {
+  return dispatch => {
     console.log('Dispatching fetchCatRequest');
     dispatch(fetchCatRequest());
     console.log('Fetching api');
     fetch(`${API_BASE_URL}/pets/cats`, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       }
     })
-    .then(res => res.json())
-    .then((results) => {
-      console.log('This is the cats get request result ==>  ' + JSON.stringify(results));
-      dispatch(fetchCatSuccess(results))
-    })
-      .catch(error => dispatch(fetchCatError(error)))
-    }
-}
+      .then(res => res.json())
+      .then(results => {
+        console.log(
+          'This is the cats get request result ==>  ' + JSON.stringify(results)
+        );
+        dispatch(fetchCatSuccess(results));
+      })
+      .catch(error => dispatch(fetchCatError(error)));
+  };
+};
 
-
-export const DELETE_CAT_SUCCESS = 'DELETE_CAT_SUCCESS'
-export const deleteCatSuccess = (results) => ({
-  type: DELETE_CAT_SUCCESS,
+export const ADOPT_CAT_SUCCESS = 'ADOPT_CAT_SUCCESS';
+export const adoptCatSuccess = results => ({
+  type: ADOPT_CAT_SUCCESS,
   results
 });
 
-export const DELETE_CAT_REQUEST = 'DELETE_CAT_REQUEST';
-export const deleteCatRequest = () => ({
-  type: DELETE_CAT_REQUEST
+export const ADOPT_CAT_REQUEST = 'ADOPT_CAT_REQUEST';
+export const adoptCatRequest = () => ({
+  type: ADOPT_CAT_REQUEST
 });
 
-export const DELETE_CAT_ERROR = 'DELETE_CAT_ERROR';
-export const deleteCatError = (error) => ({
-  type: DELETE_CAT_ERROR,
+export const ADOPT_CAT_ERROR = 'ADOPT_CAT_ERROR';
+export const adoptCatError = error => ({
+  type: ADOPT_CAT_ERROR,
   error
 });
+
+export const adoptCat = () => {
+  return dispatch => {
+    console.log('Dispatching adoptCatRequest');
+    dispatch(adoptCatRequest());
+    fetch(`${API_BASE_URL}/pets/cats`, {
+      method: 'DELETE',
+    })
+    .then(res => res.json())
+    .then(dispatch(fetchCat()))
+    .catch(error => dispatch(adoptCatError(error)));
+  };
+};
--- src/actions/dog.js
+++ src/actions/dog.js
@@ -2,8 +2,9 @@
 //TODO: NEED TO ADD THE API_BASE_URL TO CONFIG
 import { API_BASE_URL } from '../config';
 
+
 export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
-export const fetchDogSuccess = (results) => ({
+export const fetchDogSuccess = results => ({
   type: FETCH_DOG_SUCCESS,
   results
 });
@@ -14,27 +15,59 @@ export const fetchDogRequest = () => ({
 });
 
 export const FETCH_DOG_ERROR = 'FETCH_DOG_ERROR';
-export const fetchDogError = (error) => ({
+export const fetchDogError = error => ({
   type: FETCH_DOG_ERROR,
   error
 });
 
 export const fetchDog = () => {
-  return (dispatch) => {
+  return dispatch => {
     console.log('Dispatching fetchDogRequest');
     dispatch(fetchDogRequest());
     console.log('Fetching api');
     fetch(`${API_BASE_URL}/pets/Dogs`, {
       headers: {
-        'Accept': 'appliDogion/json',
-        'Content-Type': 'appliDogion/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       }
     })
       .then(res => res.json())
-      .then((results) => {
-        console.log('This is the Dogs get request result ==>  ' + JSON.stringify(results));
-        dispatch(fetchDogSuccess(results))
+      .then(results => {
+        console.log(
+          'This is the Dogs get request result ==>  ' + JSON.stringify(results)
+        );
+        dispatch(fetchDogSuccess(results));
       })
-      .catch(error => dispatch(fetchDogError(error)))
-  }
-}
+      .catch(error => dispatch(fetchDogError(error)));
+  };
+};
+
+export const ADOPT_DOG_SUCCESS = 'ADOPT_DOG_SUCCESS';
+export const adoptDogSuccess = results => ({
+  type: ADOPT_DOG_SUCCESS,
+  results
+});
+
+export const ADOPT_DOG_REQUEST = 'ADOPT_DOG_REQUEST';
+export const adoptDogRequest = () => ({
+  type: ADOPT_DOG_REQUEST
+});
+
+export const ADOPT_DOG_ERROR = 'ADOPT_DOG_ERROR';
+export const adoptDogError = error => ({
+  type: ADOPT_DOG_ERROR,
+  error
+});
+
+export const adoptDog = () => {
+  return dispatch => {
+    console.log('Dispatching adoptDogRequest');
+    dispatch(adoptDogRequest());
+    fetch(`${API_BASE_URL}/pets/Dogs`, {
+      method: 'DELETE',
+    })
+      .then(res => res.json())
+      .then(dispatch(fetchDog()))
+      .catch(error => dispatch(adoptDogError(error)));
+  };
+};
